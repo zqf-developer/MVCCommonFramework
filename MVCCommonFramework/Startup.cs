dd25399ef2aa481f5453017cde2ff6ac7806b71a
QingFengZhuo <QingFengZhuo@ZQF-PC>
2018-11-12 14:45:45 +0800

    添加项目文件。

---
+++ MVCCommonFramework/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCCommonFramework.Startup))]
+namespace MVCCommonFramework
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
